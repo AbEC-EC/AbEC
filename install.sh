466a5e7c06f64a9326de0cc2bc8b85a2bec7cf44
Alexandre Mascarenhas <43816407+mascarenhasav@users.noreply.github.com>
2023-10-05 00:05:22 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt install python3.10 python3-pip
-python3 -m venv docs/install/venv-abec
-source ./docs/install/venv-abec/bin/activate
-pip install -r ./docs/install/requirements.txt
+python3 -m venv abec/aux/install/venv-abec
+source ./abec/aux/install/venv-abec/bin/activate
+pip install -r .abec/aux/install/requirements.txt
 deactivate
