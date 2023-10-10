2f9fb5cd81e3663ec53e53257e96afe2a1a2886a
mascarenhasav <mascarenhasav@gmail.com>
2023-10-10 16:54:32 +0900

    install

--- abec/aee.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if [[ "$(python3 -V)" =~ "Python 3" ]]
-then
-    echo "Python 3 is installed"
-fi
--- install.sh
+++ install.sh
@@ -3,10 +3,14 @@ if [[ "$(python3 -V)" =~ "Python 3" ]]
 then 
     echo "Python 3 is already installed"
 else
+    echo "Python 3 is required in order to run the framework"
     echo "Installing python3 ..."
     sudo apt install python3.10 python3-pip
+fi
+
 echo "Setting the virtual enviroment up ..."
 python3 -m venv docs/install/venv-abec
 source ./docs/install/venv-abec/bin/activate
 pip install -r ./docs/install/requirements.txt
+echo "Done! now you can run the framework in the abec/ dir"
 deactivate
