8d7cb3b660e0d84fa3853529e48746d9119c1dd6
mascarenhasav <mascarenhasav@gmail.com>
2024-01-20 17:25:46 +0900

    ae

---
+++ abec/fitness_functions_ex/fitness_template.py
@@ -0,0 +1,12 @@
+
+# Function to evaluate the fitness of the individual x
+'''
+x (list) - list with size of the number of dimensions of the problem
+nevals (scalar) - Current number of fitness evaluation
+'''
+def function(x, nevals):
+    # code to evaluate the fitness
+    ...
+    ...
+    ...
+    return fitness
